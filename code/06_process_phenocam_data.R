a9849b8d1e8027fe45ea7e3684948beb90cf559f
Adam Young <7317170+amyoun01@users.noreply.github.com>
2022-08-29 21:51:48 -0700

    Update commenting.

--- code/06_process_phenocam_data.R
+++ code/06_process_phenocam_data.R
@@ -1,3 +1,10 @@
+# This script goes through and organizes and gathers PhenoCam Gcc data into what
+# is needed for the aerodynamic resistance analysis. Specifically, it identifies 
+# the Gcc statistic (e.g., mean, median, 75th percentile, 90th percentile) that
+# minimizes the RMSE. This is the Gcc time series we use. We also recalculate
+# transition dates for when the Gcc magnigutde is at 10%, 50%, and 90% percent,
+# instead of the published 10%, 25%, and 50%.
+
 rm(list=ls())
 graphics.off()
 
@@ -20,12 +27,16 @@ for (i in 1:length(phenos)){
     pheno_dir <- getwd()
     versions <- files <- list.files()
     
+    # Go through each version for each site. There are different versions depending
+    # on if the field of view of the camera changes.
     for (v in 1:length(versions)){
         
         setwd(sprintf('%s//%s',pheno_dir,versions[v]));
         file_to_read = paste0(sprintf("%s_%s_1day_transition_dates.csv",
                                       phenos[i],versions[v]))
         
+        # The next several lines reads in the RMSE data from the file 
+        # header. We find which statistic minimizes this RMSE.
         fid <- file(description = file_to_read,open = "rt")
         header_lines <- readLines(con = fid,n = 15)
         close(fid)
@@ -43,9 +54,12 @@ for (i in 1:length(phenos)){
         min_rmse <- which(rmse == min(rmse))
         min_rmse <- ifelse(length(min_rmse) > 1,min_rmse[1],min_rmse)
         
+        # Read in PhenoCam time series data for 1-day step
         pheno_td <- read_phenocam(filename = sprintf("%s_%s_1day.csv",
                                                      phenos[i],versions[v]))
         
+        # identify transition dates fro 10%, 50%, and 90% of the annual Gcc
+        # magnitude.
         new_dates <- transition_dates(pheno_td,
                                       lower_thresh = 0.1,
                                       middle_thresh = 0.5,
@@ -54,6 +68,7 @@ for (i in 1:length(phenos)){
                                       reverse = FALSE,
                                       plot = FALSE)
         
+        # Store new dates
         new_dates$direction <- "rising"
         new_dates <- new_dates[,c(ncol(new_dates),1:9)]
         new_dates[,2] <- anydate(new_dates[,2])
@@ -76,6 +91,7 @@ for (i in 1:length(phenos)){
             
         }
         
+        # Read in three day data and use only time series that minimizes RMSE
         pheno_ts_name <- paste0(sprintf("%s_%s_3day.csv",
                                         phenos[i],versions[v]))
         pheno_ts_v <- read.csv(pheno_ts_name,header = TRUE,sep = ",",skip = 24)
@@ -83,6 +99,7 @@ for (i in 1:length(phenos)){
         col_id_1 <- which(colnames(pheno_ts_v) == paste0("gcc_",rmse_stats[min_rmse]))
         col_id_2 <- which(colnames(pheno_ts_v) == paste0("smooth_gcc_",rmse_stats[min_rmse]))
         
+        # Make simple data frame to store time series data for given site
         new_ts <- data.frame(date = pheno_ts_v$date,
                              gcc = pheno_ts_v[,col_id_1],
                              smooth_gcc = pheno_ts_v[,col_id_2])
@@ -107,7 +124,8 @@ for (i in 1:length(phenos)){
     }
     
     pheno_ts[is.na(pheno_ts)] <- -9999
-    
+   
+    # Export time series
     pheno_ts_file_to_write <- sprintf("%s_gcc_time_series.csv",phenos[i])
     write.csv(pheno_ts,paste0(wdir,
                               "/results/3_processed_phenocam_data/time_series/",
